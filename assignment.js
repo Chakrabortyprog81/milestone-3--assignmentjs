8b3d08bc73aecae9efec53f60fa47b8d269249de
Chakrabortyprog81 <67548530+Chakrabortyprog81@users.noreply.github.com>
2020-07-18 20:55:39 +0600

    assignment js upload

---
+++ assignment.js
@@ -0,0 +1,22 @@
+// feetToMile
+function feetToMile(feet) {
+    var mile = feet/5280;
+    return mile;
+}
+
+
+// // woodCalculator
+function woodCalculator (chair, table, bed){
+    var chairCount = chair * 1;
+    var tableCount = table * 3;
+    var bedCount = bed * 5;
+    var totalWood = chairCount + tableCount + bedCount;
+    return totalWood;
+}
+
+
+// tinyFriend
+
+// brickCalculator
+
+
